5e9d052035c8bd2e35e80545fb33c9216f518df5
Ryely <ryely.moore@outlook.com>
2021-05-06 09:22:52 -0600

    using functions, vars, and console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight () {
+window.alert("The fight has begun!");
+}
+//fight ();
+var playerName = window.prompt ("What is your robots name?");
+console.log(playerName);
+console.log ("This logs a string, good for leaving yourself a message.");
+console.log (100+10);
+console.log ("Our robot's name is " + playerName);
